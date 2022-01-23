d87df089ba2628c272914903191cefffd12e0f83
Cajaye Clarke <cajayeclarke44@gmail.com>
2022-01-23 01:58:14 -0500

    added crate to generate random numbers

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::io; //we get input/output from the std library
+use rand::Rng;
+use std::io; //we get input/output from the std library // Rng defines methods that random number generators implement
 
 fn main() {
     println!("Guessing game!");
     println!("Enter your guess");
 
-    let mut guess = String::new(); //empty mutale string
+    //let secret_number =
+
+    let mut guess = String::new(); //empty mutable string
 
     io::stdin().read_line(&mut guess).expect("Cannot read line"); // & references a variable and &mut references a mutabe variable
 
